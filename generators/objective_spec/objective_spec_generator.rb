049e2eccb726b34870c86defa0ca28e427b39636
Jeremy Ellison <jeremy@3cubetechnologies.com>
2009-07-02 13:57:24 -0400

    first cut at generators

--- generators/objective_spec/objective_spec_generator.rb
+++ generators/objective_spec/objective_spec_generator.rb
@@ -1,32 +1,173 @@
-class ObjectiveSpecGenerator < Rails::Generator::Base
-  
-  def initialize(runtime_args, runtime_options = {})      
-    puts <<-INFO
+class ObjectiveSpecGenerator < Rails::Generator::NamedBase
+  attr_reader   :controller_name,
+                :controller_class_path,
+                :controller_file_path,
+                :controller_class_nesting,
+                :controller_class_nesting_depth,
+                :controller_class_name,
+                :controller_singular_name,
+                :controller_plural_name,
+                :resource_edit_path,
+                :default_file_extension,
+                :generator_default_file_extension
+  alias_method  :controller_file_name,  :controller_singular_name
+  alias_method  :controller_table_name, :controller_plural_name
 
-You are installing Objective Spec. After this generator has completed, you
-will need to add require 'objective_spec' to your spec/spec_helper.rb file
-to load the library.
-
-INFO
+  def initialize(runtime_args, runtime_options = {})
     super
-  end
-  
-  def manifest
-    record do |m|
-      m.directory 'spec/mailers'
-      m.directory 'spec/matchers'
-      m.directory 'spec/spec_helpers'
-      m.file      'spec_helpers/common.rb',       'spec/spec_helpers/common.rb'
-      m.file      'spec_helpers/model.rb',        'spec/spec_helpers/model.rb'
-      m.file      'spec_helpers/view.rb',         'spec/spec_helpers/view.rb'
-      m.file      'spec_helpers/controller.rb',   'spec/spec_helpers/controller.rb'
+
+    if has_rspec?
+      if ActionController::Base.respond_to?(:resource_action_separator)
+        @resource_edit_path = "/edit"
+      else
+        @resource_edit_path = ";edit"
+      end
+    else
+      raise "You do not have rspec installed. WTF are you doing?"
+    end
+    
+    raise "You don't have haml installed, WTF are you, a joker?" unless defined?(Haml)
+    @generator_default_file_extension = "haml"
+    @default_file_extension = "html.#{@generator_default_file_extension}"
+    
+    @controller_name = @name.pluralize
+
+    base_name, @controller_class_path, @controller_file_path, @controller_class_nesting, @controller_class_nesting_depth = extract_modules(@controller_name)
+    @controller_class_name_without_nesting, @controller_singular_name, @controller_plural_name = inflect_names(base_name)
+
+    if @controller_class_nesting.empty?
+      @controller_class_name = @controller_class_name_without_nesting
+    else
+      @controller_class_name = "#{@controller_class_nesting}::#{@controller_class_name_without_nesting}"
     end
   end
-  
-protected
 
-  def banner
-    "Usage: #{$0} objective_spec"
+  def manifest
+    record do |m|
+      # Check for class naming collisions.
+      m.class_collisions(controller_class_path, "#{controller_class_name}Controller", "#{controller_class_name}Helper")
+      m.class_collisions(class_path, "#{class_name}")
+
+      # Controller, helper, views, and test directories.
+      m.directory(File.join('app/models', class_path))
+      m.directory(File.join('app/controllers', controller_class_path))
+      m.directory(File.join('app/helpers', controller_class_path))
+      m.directory(File.join('app/views', controller_class_path, controller_file_name))
+
+      m.directory(File.join('spec/controllers', controller_class_path))
+      m.directory(File.join('spec/helpers', class_path))
+      m.directory(File.join('spec/models', class_path))
+      m.directory File.join('spec/views', controller_class_path, controller_file_name)
+      m.directory(File.join('spec/fixtures', class_path))
+      
+      scaffold_views.each do |action|
+        m.template(
+          "view_#{action}.#{generator_default_file_extension}",
+          File.join('app/views', controller_class_path, controller_file_name, "#{action}.#{default_file_extension}")
+        )
+      end
+
+      m.template('model.rb', File.join('app/models', class_path, "#{file_name}.rb"))
+      m.template('controller.rb', File.join('app/controllers', controller_class_path, "#{controller_file_name}_controller.rb"))
+      m.template('helper.rb',          File.join('app/helpers',     controller_class_path, "#{controller_file_name}_helper.rb"))
+
+      m.template('rspec/functional_spec.rb',  File.join('spec/controllers', controller_class_path, "#{controller_file_name}_controller_spec.rb"))
+      m.template('rspec/routing_spec.rb',     File.join('spec/controllers', controller_class_path, "#{controller_file_name}_routing_spec.rb"))
+      m.template('rspec/helper_spec.rb',      File.join('spec/helpers',     class_path, "#{controller_file_name}_helper_spec.rb"))
+      m.template('rspec/unit_spec.rb',        File.join('spec/models',      class_path, "#{file_name}_spec.rb"))
+      
+      #Use Factories instead of fixtures
+      model_factory = "\n\nFactory.define(:#{model_name}) do |f|\n"
+      for attribute in attributes
+        model_factory << "#{attribute.name} #{attribute.default}\n"
+      end
+      model_factory << "end\n"
+      append_file("spec/factories.rb", model_factory)
+
+      rspec_views.each do |action|
+        m.template(
+          "rspec/views/#{action}_spec.rb",
+          File.join('spec/views', controller_class_path, controller_file_name, "#{action}_spec.rb")
+        )
+      end
+
+
+      unless options[:skip_migration]
+        migration_template = 'migration.rb'
+        
+        m.migration_template(
+          migration_template, 'db/migrate', 
+          :assigns => {
+            :migration_name => "Create#{class_name.pluralize.gsub(/::/, '')}",
+            :attributes     => attributes
+          }, 
+          :migration_file_name => "create_#{file_path.gsub(/\//, '_').pluralize}"
+        )
+      end
+
+      m.route_resources controller_file_name
+    end
+  end
+
+  # Lifted from Rick Olson's restful_authentication
+  def has_rspec?
+    options[:rspec] || (File.exist?('spec') && File.directory?('spec'))
   end
   
+  protected
+    # Override with your own usage banner.
+    def banner
+      "Usage: #{$0} scaffold_resource ModelName [field:type, field:type]"
+    end
+
+    def rspec_views
+      %w[ index show new edit ]
+    end
+    
+    def scaffold_views
+      rspec_views + %w[ _form ]
+    end
+
+    def model_name 
+      class_name.demodulize
+    end
+
+    def add_options!(opt)
+      opt.separator ''
+      opt.separator 'Options:'
+      opt.on("--rspec", "Force rspec mode (checks for RAILS_ROOT/spec by default)") { |v| options[:rspec] = true }
+    end
+    
+    def append_file(path, data)
+      File.open(path, 'ab') { |file| file.write(data) }
+    end
+end
+
+module Rails
+  module Generator
+    class GeneratedAttribute
+      def default_value
+        @default_value ||= case type
+          when :int, :integer               then "\"1\""
+          when :float                       then "\"1.5\""
+          when :decimal                     then "\"9.99\""
+          when :datetime, :timestamp, :time then "Time.now"
+          when :date                        then "Date.today"
+          when :string                      then "\"MyString\""
+          when :text                        then "\"MyText\""
+          when :boolean                     then "false"
+          else
+            ""
+        end      
+      end
+
+      def input_type
+        @input_type ||= case type
+          when :text                        then "textarea"
+          else
+            "input"
+        end      
+      end
+    end
+  end
 end
