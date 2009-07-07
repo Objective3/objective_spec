4dd304c357bbb3845eb2c1877525e6714b2df993
Blake Watters <blake@objective3.com>
2009-07-07 13:18:05 -0400

    Updating spec generator to be more idiomatic

--- generators/objective_resource/templates/rspec/functional_spec.rb
+++ generators/objective_resource/templates/rspec/functional_spec.rb
@@ -4,127 +4,127 @@ describe <%= controller_class_name %>Controller do
   describe "handling GET /<%= table_name %>" do
   
     it "should be successful" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return([@<%= file_name %>])
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
       get :index
       response.should be_success
     end
 
     it "should render index template" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return([@<%= file_name %>])
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
       get :index
       response.should render_template('index')
     end
   
     it "should find all <%= table_name %>" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:find).with(:all).and_return([@<%= file_name %>])
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.should_receive(:find).with(:all).and_return([<%= file_name %>])
       get :index
     end
   
     it "should assign the found <%= table_name %> for the view" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return([@<%= file_name %>])
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
       get :index
-      assigns[:<%= table_name %>].should == [@<%= file_name %>]
+      assigns[:<%= table_name %>].should == [<%= file_name %>]
     end
   end
 
   describe "handling GET /<%= table_name %>/1" do
 
     it "should be successful" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :show, :id => "1"
       response.should be_success
     end
   
     it "should render show template" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :show, :id => "1"
       response.should render_template('show')
     end
   
     it "should find the <%= file_name %> requested" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
       get :show, :id => "1"
     end
   
     it "should assign the found <%= file_name %> for the view" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :show, :id => "1"
-      assigns[:<%= file_name %>].should equal(@<%= file_name %>)
+      assigns[:<%= file_name %>].should equal(<%= file_name %>)
     end
   end
 
   describe "handling GET /<%= table_name %>/new" do
 
     it "should be successful" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
       get :new
       response.should be_success
     end
   
     it "should render new template" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
       get :new
       response.should render_template('new')
     end
   
     it "should create an new <%= file_name %>" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:new).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.should_receive(:new).and_return(<%= file_name %>)
       get :new
     end
   
     it "should not save the new <%= file_name %>" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
-      @<%= file_name %>.should_not_receive(:save)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+      <%= file_name %>.should_not_receive(:save)
       get :new
     end
   
     it "should assign the new <%= file_name %> for the view" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
       get :new
-      assigns[:<%= file_name %>].should equal(@<%= file_name %>)
+      assigns[:<%= file_name %>].should equal(<%= file_name %>)
     end
   end
 
   describe "handling GET /<%= table_name %>/1/edit" do
 
     it "should be successful" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :edit, :id => "1"
       response.should be_success
     end
   
     it "should render edit template" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :edit, :id => "1"
       response.should render_template('edit')
     end
   
     it "should find the <%= file_name %> requested" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.should_receive(:find).and_return(<%= file_name %>)
       get :edit, :id => "1"
     end
   
     it "should assign the found <%= controller_class_name %> for the view" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :edit, :id => "1"
-      assigns[:<%= file_name %>].should equal(@<%= file_name %>)
+      assigns[:<%= file_name %>].should equal(<%= file_name %>)
     end
   end
 
@@ -133,18 +133,18 @@ describe <%= controller_class_name %>Controller do
     describe "with successful save" do
   
       it "should create a new <%= file_name %>" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.should_receive(:new).with({}).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:save).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.should_receive(:new).with({}).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:save).and_return(true)
         post :create, :<%= file_name %> => {}
       end
 
       it "should redirect to the new <%= file_name %>" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:save).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:save).and_return(true)
         post :create, :<%= file_name %> => {}
         response.should redirect_to(<%= table_name.singularize %>_url("1"))
       end
@@ -154,10 +154,10 @@ describe <%= controller_class_name %>Controller do
     describe "with failed save" do
   
       it "should re-render 'new'" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:new).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:save).and_return(false)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:save).and_return(false)
         post :create, :<%= file_name %> => {}
         response.should render_template('new')
       end
@@ -170,36 +170,36 @@ describe <%= controller_class_name %>Controller do
     describe "with successful update" do
 
       it "should find the <%= file_name %> requested" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:update_attributes).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1"
       end
 
       it "should update the found <%= file_name %>" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:update_attributes).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1"
-        assigns(:<%= file_name %>).should equal(@<%= file_name %>)
+        assigns(:<%= file_name %>).should equal(<%= file_name %>)
       end
 
       it "should assign the found <%= file_name %> for the view" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:update_attributes).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1"
-        assigns(:<%= file_name %>).should equal(@<%= file_name %>)
+        assigns(:<%= file_name %>).should equal(<%= file_name %>)
       end
 
       it "should redirect to the <%= file_name %>" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:update_attributes).and_return(true)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1"
         response.should redirect_to(<%= table_name.singularize %>_url("1"))
       end
@@ -209,10 +209,10 @@ describe <%= controller_class_name %>Controller do
     describe "with failed update" do
 
       it "should re-render 'edit'" do
-        @<%= file_name %> = Factory(:<%= file_name %>)
-        @<%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
-        @<%= file_name %>.should_receive(:update_attributes).and_return(false)
+        <%= file_name %> = Factory(:<%= file_name %>)
+        <%= file_name %>.stub!(:to_param).and_return("1")
+        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= file_name %>.should_receive(:update_attributes).and_return(false)
         put :update, :id => "1"
         response.should render_template('edit')
       end
@@ -223,23 +223,23 @@ describe <%= controller_class_name %>Controller do
   describe "handling DELETE /<%= table_name %>/1" do
 
     it "should find the <%= file_name %> requested" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      @<%= file_name %>.stub!(:destroy).and_return(true)
-      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= file_name %>.stub!(:destroy).and_return(true)
+      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
       delete :destroy, :id => "1"
     end
   
     it "should call destroy on the found <%= file_name %>" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
-      @<%= file_name %>.should_receive(:destroy).and_return(true) 
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+      <%= file_name %>.should_receive(:destroy).and_return(true) 
       delete :destroy, :id => "1"
     end
   
     it "should redirect to the <%= table_name %> list" do
-      @<%= file_name %> = Factory(:<%= file_name %>)
-      @<%= file_name %>.stub!(:destroy).and_return(true)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(@<%= file_name %>)
+      <%= file_name %> = Factory(:<%= file_name %>)
+      <%= file_name %>.stub!(:destroy).and_return(true)
+      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       delete :destroy, :id => "1"
       response.should redirect_to(<%= table_name %>_url)
     end
--- generators/objective_resource/templates/rspec/unit_spec.rb
+++ generators/objective_resource/templates/rspec/unit_spec.rb
@@ -1,11 +1,10 @@
 require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * controller_class_nesting_depth %>/../spec_helper')
 
 describe <%= class_name %> do
-  before(:each) do
-    @<%= file_name %> = <%= class_name %>.new
-  end
 
   it "should be valid" do
-    @<%= file_name %>.should be_valid
+    <%= file_name %> = Factory(:<%= file_name %>)
+    <%= file_name %>.should be_valid
   end
+  
 end
