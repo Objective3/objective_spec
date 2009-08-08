adb0246a3906ddf2f5b33e39903b8b7ee8c5808c
Blake Watters <blake@objective3.com>
2009-08-08 04:23:50 -0400

    hrm.

--- generators/objective_service/templates/controller_spec.rb
+++ generators/objective_service/templates/controller_spec.rb
@@ -1,24 +1,24 @@
-require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * controller_class_nesting_depth %>/../spec_helper')
+require File.expand_path(File.dirname(__FILE__) + '<%= '/..' * class_nesting_depth %>/../spec_helper')
 
-describe <%= controller_class_name %>Controller do
+describe <%= class_name %>Controller do
   describe "handling GET /<%= table_name %>" do
   
     it "should be successful" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
+      <%= class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
       get :index, :format => 'xml'
       response.should be_success
     end
   
     it "should find all <%= table_name %>" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:find).with(:all).and_return([<%= file_name %>])
+      <%= class_name.singularize %>.should_receive(:find).with(:all).and_return([<%= file_name %>])
       get :index, :format => 'xml'
     end
   
     it "should assign the found <%= table_name %> for the view" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
+      <%= class_name.singularize %>.stub!(:find).and_return([<%= file_name %>])
       get :index, :format => 'xml'
       assigns[:<%= table_name %>].should == [<%= file_name %>]
     end
@@ -28,20 +28,20 @@ describe <%= controller_class_name %>Controller do
 
     it "should be successful" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+      <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :show, :id => "1", :format => 'xml'
       response.should be_success
     end
   
     it "should find the <%= file_name %> requested" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
+      <%= class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
       get :show, :id => "1", :format => 'xml'
     end
   
     it "should assign the found <%= file_name %> for the view" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+      <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       get :show, :id => "1", :format => 'xml'
       assigns[:<%= file_name %>].should equal(<%= file_name %>)
     end
@@ -54,7 +54,7 @@ describe <%= controller_class_name %>Controller do
       it "should create a new <%= file_name %>" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.should_receive(:new).with({}).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.should_receive(:new).with({}).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:save).and_return(true)
         post :create, :<%= file_name %> => {}, :format => 'xml'
       end
@@ -62,7 +62,7 @@ describe <%= controller_class_name %>Controller do
       it "should be successful" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:save).and_return(true)
         post :create, :<%= file_name %> => {}, :format => 'xml'
         response.should be_success
@@ -75,7 +75,7 @@ describe <%= controller_class_name %>Controller do
       it "should be unprocessable" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:save).and_return(false)
         post :create, :<%= file_name %> => {}, :format => 'xml'
         response.code.should == '422'
@@ -84,7 +84,7 @@ describe <%= controller_class_name %>Controller do
       it "should return errors" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:new).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:save).and_return(false)
         post :create, :<%= file_name %> => {}, :format => 'xml'
         Nokogiri::XML(response.should).at('errors').should_not be_nil
@@ -100,7 +100,7 @@ describe <%= controller_class_name %>Controller do
       it "should find the <%= file_name %> requested" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
+        <%= class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1", :format => 'xml'
       end
@@ -108,7 +108,7 @@ describe <%= controller_class_name %>Controller do
       it "should update the found <%= file_name %>" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1", :format => 'xml'
         assigns(:<%= file_name %>).should equal(<%= file_name %>)
@@ -117,7 +117,7 @@ describe <%= controller_class_name %>Controller do
       it "should assign the found <%= file_name %> for the view" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1", :format => 'xml'
         assigns(:<%= file_name %>).should equal(<%= file_name %>)
@@ -126,7 +126,7 @@ describe <%= controller_class_name %>Controller do
       it "should be successful" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:update_attributes).and_return(true)
         put :update, :id => "1", :format => 'xml'
         response.should be_success
@@ -139,7 +139,7 @@ describe <%= controller_class_name %>Controller do
       it "should render errors" do
         <%= file_name %> = Factory(:<%= file_name %>)
         <%= file_name %>.stub!(:to_param).and_return("1")
-        <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+        <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
         <%= file_name %>.should_receive(:update_attributes).and_return(false)
         put :update, :id => "1", :format => 'xml'
         Nokogiri::XML(response.body).at('errors').should_not be_nil
@@ -153,13 +153,13 @@ describe <%= controller_class_name %>Controller do
     it "should find the <%= file_name %> requested" do
       <%= file_name %> = Factory(:<%= file_name %>)
       <%= file_name %>.stub!(:destroy).and_return(true)
-      <%= controller_class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
+      <%= class_name.singularize %>.should_receive(:find).with("1").and_return(<%= file_name %>)
       delete :destroy, :id => "1", :format => 'xml'
     end
   
     it "should call destroy on the found <%= file_name %>" do
       <%= file_name %> = Factory(:<%= file_name %>)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+      <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       <%= file_name %>.should_receive(:destroy).and_return(true) 
       delete :destroy, :id => "1", :format => 'xml'
     end
@@ -167,7 +167,7 @@ describe <%= controller_class_name %>Controller do
     it "should succeed" do
       <%= file_name %> = Factory(:<%= file_name %>)
       <%= file_name %>.stub!(:destroy).and_return(true)
-      <%= controller_class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
+      <%= class_name.singularize %>.stub!(:find).and_return(<%= file_name %>)
       delete :destroy, :id => "1", :format => 'xml'
       response.should be_success
     end
